3246759d95ca8426468392500423f2b0efca1b35
Webierta <webierta@gmail.com>
2019-03-09 23:41:39 +0100

    preferences

--- app/src/main/java/com/ahorcado/MiJuego.kt
+++ app/src/main/java/com/ahorcado/MiJuego.kt
@@ -3,7 +3,7 @@ package com.ahorcado
 data class MiJuego (
     var victorias: Int = 0,
     var derrotas: Int = 0,
-    var nivel: Int = 0,
+    var modo: String = "",
     var sonido: Boolean = true,
     var texto: String = "",
     var version: String = ""
--- app/src/main/java/com/ahorcado/SettingsActivity.kt
+++ app/src/main/java/com/ahorcado/SettingsActivity.kt
@@ -6,6 +6,84 @@ import android.net.ConnectivityManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Gravity
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.ahorcado.databinding.PreferencesBinding
+
+class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: PreferencesBinding
+    private val miJuego = MiJuego()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        binding = DataBindingUtil.setContentView(this, R.layout.preferences)
+        binding.miJuego = miJuego
+
+        val niveles = arrayOf("Avanzado", "Júnior", "Temas")
+        val adaptador = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, niveles)
+        binding.nivel.adapter = adaptador
+
+        val preferencias = getSharedPreferences("datos", Context.MODE_PRIVATE)
+        val nivel = preferencias.getString("modo", "Avanzado")
+        val sonido: Boolean = preferencias.getBoolean("sound", true)
+        miJuego.modo = nivel?: "Avanzado"
+        miJuego.sonido = sonido
+
+        binding.sonido.isChecked = miJuego.sonido
+        binding.textNivel.text = miJuego.modo
+
+        binding.nivel.onItemClickListener = AdapterView.OnItemClickListener { parent, _, position, _ ->
+            val selectedItem = parent.getItemAtPosition(position) as String
+            binding.textNivel.text = selectedItem
+        }
+
+        binding.btnGuardar.setOnClickListener {
+
+            miJuego.modo = (binding.textNivel.text).toString()
+            miJuego.sonido = binding.sonido.isChecked
+
+            val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val activeNetwork = cm.activeNetworkInfo
+            val isConnected = activeNetwork != null && activeNetwork.isConnected
+
+            val editor = preferencias.edit()
+            val nivelControl= if (!isConnected) "Temas" else miJuego.modo
+            /*if (!isConnected) {
+                editor.putString("modo", "Temas")
+            } else {
+                editor.putString("modo", miJuego.modo)
+            }*/
+            editor.putString("modo", nivelControl)
+            editor.putBoolean("sound", miJuego.sonido)
+            editor.apply()
+
+            if (!isConnected && nivel != "Temas") {
+                val toastInternet = Toast.makeText(this, "El nivel $nivel requiere conexión a internet.\nCambiando a Temas", Toast.LENGTH_LONG)
+                toastInternet.setGravity(Gravity.TOP or Gravity.CENTER, 0, 550)
+                toastInternet.show()
+                miJuego.modo = "Temas"
+            } else {
+                Toast.makeText(this, "Ajustes guardados", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+
+        binding.btnVolver.setOnClickListener {
+            finish()
+        }
+    }
+}
+/*
+
+import android.content.Context
+import android.databinding.DataBindingUtil
+import android.net.ConnectivityManager
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Gravity
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.ahorcado.databinding.ActivitySettingsBinding
@@ -77,3 +155,4 @@ class SettingsActivity : AppCompatActivity() {
         }
     }
 }
+*/
